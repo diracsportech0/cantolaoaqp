7100f65ae682f9bc4ffb1bf2ec18007e5575a583
diracsportech0 <dirac.sportech@gmail.com>
2026-02-20 14:48:37 -0500

    borras info de pases, no tenemos datos de pases

--- pages/2_🎖️Mi_Club.py
+++ pages/2_🎖️Mi_Club.py
@@ -7,7 +7,7 @@ from mplsoccer import (VerticalPitch, Pitch, create_transparent_cmap,
                        FontManager, arrowhead_marker, Sbopen)
 
 #from Home_page import name_club, id_club
-from etl import df, df_pass #df_tipo1, df_tipo2
+from etl import df #df_pass #df_tipo1, df_tipo2
 from functions import barras_apiladas, tipo_tiros_goles, mostrar_tablas_zonas, mapa_pases
 
 
@@ -62,23 +62,7 @@ if choice2 == 'Informe de partido':
         mostrar_tablas_zonas(df, defensa)
     
     if menu_subinforme == 'Pases':
-        col1n, col2n = st.columns(2)
-        with col1n:
-            mapa_pases(df_pass[df_pass.zone=='1er tercio'],'CORRECTO','USMP')
-        with col2n:
-            mapa_pases(df_pass[df_pass.zone=='1er tercio'],'ERRONEO','USMP')
-        
-        col3n, col4n = st.columns(2)
-        with col3n:
-            mapa_pases(df_pass[df_pass.zone=='2do tercio'],'CORRECTO','USMP')
-        with col4n:
-            mapa_pases(df_pass[df_pass.zone=='2do tercio'],'ERRONEO','USMP')
-        
-        col5n, col6n = st.columns(2)
-        with col5n:
-            mapa_pases(df_pass[df_pass.zone=='3er tercio'],'CORRECTO','USMP')
-        with col6n:
-            mapa_pases(df_pass[df_pass.zone=='3er tercio'],'ERRONEO','USMP')
+        pass
         
 
 if choice2 == 'Informe acumulado':
--- pages/3_🚹Mi_Plantel.py
+++ pages/3_🚹Mi_Plantel.py
@@ -1,30 +1,12 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from etl import df, df_pass
+from etl import df#, df_pass
 from functions import player_passmap,graph_barras, heat_map
 
 #----------------------
 
-#MENU LATERAL
-players = set(df_pass['player'].values)
-player = st.sidebar.selectbox(
-        "Jugador",
-        players,
-        0)
+st.write('NO DIDSPONIBLE')
 
 
-st.title("🚹 MI PLANTILLA")
-#--- CONTENIDO INFO BASICA
-
-df_player = df[df.player==player]
-
-######## --------------- MAPA DE PASES
-df_pass_player = df_pass[df_pass.player==player]
-#df_pass_player = df_pass_player[df_pass_player.match_filter==rival]
-player_passmap(df_pass_player,player,'USMP')
-
-metricas = ['PASE','CONDUCCION','REGATE','TIRO','DUELO AEREO','PRESION','DUELO DEFENSIVO','PERDIDA'
-            'DUELO BALON SUELTO','INTERCEPTACION','DESPEJE']
-heat_map(df_player, metricas)
 
