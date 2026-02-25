a7a3182fb7b545adfdbf095d62d94331bf73ea12
diracsportech0 <dirac.sportech@gmail.com>
2026-02-25 13:08:24 -0500

    rival por Rival en columna

--- pages/3_🚹Mi_Plantel.py
+++ pages/3_🚹Mi_Plantel.py
@@ -9,7 +9,7 @@ df_players_excel = pd.read_excel('players.xlsx')
 
 #OBTENER DATA TOTALIZADA POR JUGADOR
 # Agrupar por Partido, Jugador y Evento y contar ocurrencias
-grouped = df.groupby(['rival', 'player', 'Event']).size().reset_index(name='count')
+grouped = df.groupby(['Rival', 'player', 'Event']).size().reset_index(name='count')
 # Pivotar los datos para transformar los eventos en columnas
 pivot_table = grouped.pivot_table(index=['rival', 'player'], columns='Event', values='count', fill_value=0)
 # Restablecer el índice para que Partido y Jugador sean columnas nuevamente
