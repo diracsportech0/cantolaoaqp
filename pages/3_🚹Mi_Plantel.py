fad3f1793530b83eef77f01d8d30868009107798
diracsportech0 <dirac.sportech@gmail.com>
2026-02-24 23:57:33 -0500

    add heatmap y passmap individual & excel events indiv F1

--- pages/3_🚹Mi_Plantel.py
+++ pages/3_🚹Mi_Plantel.py
@@ -1,12 +1,33 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from etl import df#, df_pass
+from etl import df, df_pass
 from functions import player_passmap,graph_barras, heat_map
 
 #----------------------
 
-st.write('NO DISPONIBLE')
+#MENU LATERAL
+players = set(df_pass['player'].values)
+player = st.sidebar.selectbox(
+        "Jugador",
+        players,
+        0)
+
+
+st.title("🚹 MI PLANTILLA")
+#--- CONTENIDO INFO BASICA
+
+df_player = df[df.player==player]
+
+######## --------------- MAPA DE PASES
+df_pass_player = df_pass[df_pass.player==player]
+#df_pass_player = df_pass_player[df_pass_player.match_filter==rival]
+player_passmap(df_pass_player,player,'USMP')
+
+metricas = ['PASE','CONDUCCION','REGATE','TIRO','PERDIDA',
+            'PRESION','DUELO',
+            'INTERCEPTACION','DESPEJE', 'RECUPERACION']
+heat_map(df_player, metricas)
 
 
 
